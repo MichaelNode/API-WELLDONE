b1714f6ec36661c673711a135f098279186b7110
Alejandro Capparelli <alejandro.capparelli@gmail.com>
2019-03-11 22:14:55 +0100

    Create React App for adminPanel on /public/admin

---
+++ public/admin/src/AdminPanel.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class AdminPanel extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Welldone Admin Panel</h1>
+      </div>
+    );
+  }
+}
+
+export default AdminPanel;
