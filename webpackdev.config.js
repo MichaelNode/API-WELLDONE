ec8c5184479e841aa40f19fb9ce56857da4ed8b8
MichaelNode <michael.hidalgo.rios@hotmail.com>
2019-03-13 23:28:24 -0300

    add webpacks

---
+++ webpackdev.config.js
@@ -0,0 +1,36 @@
+
+
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+
+module.exports = {
+    mode: 'development',
+    module: {
+       rules: [
+         {
+           test: /\.js$/,
+           exclude: /node_modules/,
+           use: {
+             loader: "babel-loader"
+           }
+         },
+         {
+            test: /\.scss$/,
+            use: [{
+                loader: "style-loader"
+            }, {
+                loader: "css-loader"
+            }, {
+                loader: "sass-loader"
+            }]
+           }
+       ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // both options are optional
+            filename: "[name].css",
+            chunkFilename: "[id].css"
+    })
+    ]
+ }
