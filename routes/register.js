6dfe7eb80417b58d7c13a8b4d91680b31c8f3c96
Alejandro Capparelli <alejandro.capparelli@gmail.com>
2019-03-16 21:40:14 +0100

    Create validations for registration form

--- routes/register.js
+++ routes/register.js
@@ -1,21 +1,36 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/user')
+const { validationResult } = require('express-validator/check');
+const validations = require('../lib/validationsRegisterForm');
+
+const User = require('../models/user');
+
 
 router.get('/', function(req, res, next) {
   res.render('register');
 });
 
-router.post('/', async function (req, res, next) {
+router.post('/',  validations, async function (req, res, next) {
+
   try {
+    const validationErrors = validationResult(req)
+    if (!validationErrors.isEmpty()) {
+      const errorMessage = validationErrors.array()[0].msg
+      res.render('register', {message: errorMessage})
+    } 
     const userData = req.body;
     userData.password = await User.hashPassword(userData.password); 
     const newUser = new User(userData);
     const newUserSaved = await newUser.save();
-    res.json({success: true, user: newUserSaved});
+    res.render('register', {message: `${newUserSaved.name}: ¡Te has dado de alta con éxito!`});
+
+
   } catch (err){
-    res.render('register', {error: 'Error al ingresar los datos'});
+
+    res.render('register', {message: 'Hubo un error al ingresar los datos' });
+
   }
+
 })
 
 module.exports = router;
