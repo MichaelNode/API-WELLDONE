d879cb2f61202ba84dd2fd284aabfa0dccd56b2b
Inma Ordóñez <iordonezmarvizon@gmail.com>
2019-03-17 20:34:00 +0100

    build and start fix

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var bootstrap = require('bootstrap');
 var session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
 
