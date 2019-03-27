fbd09265f29c9202d32aca3900dc45143c779d74
michael <michael.hidalgo.rios@hotmail.com>
2019-03-26 23:25:02 -0300

    update

--- admin/src/components/nav/Nav.js
+++ admin/src/components/nav/Nav.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import Logout from '../login/Logout';
 import DeleteUser from '../deleteUser/deleteUser';
+import AddArticle from '../article/add_article/addarticle'
 
 export default class Nav extends Component{
     render(){
@@ -12,6 +13,7 @@ export default class Nav extends Component{
                             <Logout/>
                         </a>
                     </li>
+                   
                     <li className="nav-item">
                         <a href="#">
                             <DeleteUser/>
--- routes/apiv1/router.js
+++ routes/apiv1/router.js
@@ -4,12 +4,14 @@
 const namedRoutes = require('../../lib/namedRoutes');
 const loginRouter = require('./login');
 const deleteUserRouter = require('./deleteUser');
+const articleRouter = require('./article');
 
 // Router class
 class Router {
     constructor(app) {
         app.use(namedRoutes.apiv1_user, loginRouter);
         app.use(namedRoutes.apiv1_delete_user, deleteUserRouter);
+        app.use(namedRoutes.apiv1_article , articleRouter);
     }
 }
 
