1c66a4a2a7ac7be64125821c95c5f2297c57a257
MichaelNode <michael.hidalgo.rios@hotmail.com>
2019-04-02 10:03:11 -0300

    update

--- routes/apiv1/article.js
+++ routes/apiv1/article.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router();
 const Article = require('../../models/article');
 const upload = require('../../lib/uploadConfig');
-const generateImages = require('generate-responsive-images');
+
 
 
 
