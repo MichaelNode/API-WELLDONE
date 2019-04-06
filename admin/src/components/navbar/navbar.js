204e66c569c4ccb4364653366dbce01459ed8a28
Alejandro Capparelli <alejandro.capparelli@gmail.com>
2019-04-06 13:59:18 +0200

    Cambio de sitio el boton de borrar cuenta y verifico que el usuario logueado es el mismo que el que se está borrando

--- admin/src/components/navbar/navbar.js
+++ admin/src/components/navbar/navbar.js
@@ -9,6 +9,7 @@ import Nav from 'react-bootstrap/Nav'
 import Dropdown from 'react-bootstrap/Dropdown'
 import PropTypes from "prop-types";
 import Logout from '../login/Logout';
+import DeleteUser from '../deleteUser/deleteUser';
 // Be sure to include styles at some point, probably during your bootstraping
 
 
@@ -27,6 +28,7 @@ export default class MainNavbar extends Component {
                 <Dropdown.Menu>
                   <Dropdown.Header>{this.context.t("User")}</Dropdown.Header>
                   <Dropdown.Item href="/admin/update"> {this.context.t("Update_User")}</Dropdown.Item>
+                  <Dropdown.Item > <DeleteUser/> </Dropdown.Item>
                   <Dropdown.Item>  <Logout/> </Dropdown.Item>
                 </Dropdown.Menu>
               </Dropdown>
