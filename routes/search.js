38c066043773f598b39dd23f1341b48c86d12802
Miguel <miguel@sopinet.com>
2019-04-06 11:15:38 +0200

    Added order in search articles

--- routes/search.js
+++ routes/search.js
@@ -5,30 +5,41 @@ const express = require('express');
 const router = express.Router();
 const Article = require('../models/article');
 const User = require('../models/user');
-const { pagination } = require('../lib/utils');
+const {pagination} = require('../lib/utils');
 
 router.get('/:type?/:page?', async (req, res, next) => {
-    // get model for search
-    const filteredModel = req.params.type === 'user' ? User : Article;
-    const sort = filteredModel === User ? 'followers' : 'publi_date';
-    const filters = {};
-    const filter = filteredModel === User ? 'nick_name' : 'title';
-    const type = filteredModel === User ? 'user' : 'article';
-    const page = parseInt(req.params.page) || 1;
-    const recordsPerPage = 15;
-    const text = req.query.text;
+  // get model for search
+  const filteredModel = req.params.type === 'user' ? User : Article;
+  // get sort field ASC or DESC
+  const sortField = filteredModel === User ? 'followers' : 'publi_date';
+  const order = req.query.order || '-1';
+  const sort = {};
+  sort[sortField] = order;
+  // add filters
+  const filters = {};
+  const filter = filteredModel === User ? 'nick_name' : 'title';
+  // get type
+  const type = filteredModel === User ? 'user' : 'article';
+  // get paginator required fields
+  const page = parseInt(req.params.page) || 1;
+  const recordsPerPage = 15;
+  // get search text
+  const text = req.query.text;
 
-    // get models that have a coincidence
-    filters[filter] = new RegExp(text, 'i');
-    const models = await filteredModel.list(filters, sort, page, recordsPerPage);
-    const count = await filteredModel.Count(filters);
-    const paginator = pagination(models, count, page, recordsPerPage);
-    res.render('search/search', {
-        models: models,
-        searchType: type,
-        searchText: text,
-        ...paginator,
-    });
+  // get models that have a coincidence
+  filters[filter] = new RegExp(text, 'i');
+  const models = await filteredModel.list(filters, sort, page, recordsPerPage);
+  const count = await filteredModel.Count(filters);
+  // create paginator
+  const paginator = pagination(models, count, page, recordsPerPage);
+
+  res.render('search/search', {
+    models,
+    searchType: type,
+    searchText: text,
+    order,
+    ...paginator,
+  });
 });
 
 module.exports = router;
