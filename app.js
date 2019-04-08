f83d5928c5182fafde71eab92d0126047398f638
miguel <miguel@sopinet.com>
2019-04-08 18:59:39 +0200

    Added social share articles and fix .env

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var app = express();
 app.locals.moment = require('moment');
 require('./lib/connectMongoose');
 
-  
+
 
 
 // view engine setup
@@ -25,7 +25,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 /* var bodyParser = require('body-parser');            a
-pp.use(bodyParser.json({limit:'50mb'})); 
+pp.use(bodyParser.json({limit:'50mb'}));
 app.use(bodyParser.urlencoded({extended:true, limit:'50mb'}));
 app.use(express.json({limit: '50mb'})); */
 
@@ -62,6 +62,7 @@ app.use(async (req, res, next) => {
   res.locals.isLogged = require('./lib/jwtAuth').isLogged(req);
   const port = req.app.settings.port || 3000  ;
   res.locals.requested_url = req.protocol + '://' + req.host  + ( port == 80 || port == 443 ? '' : ':'+port ) + req.path;
+  res.locals.facebook_app_id = process.env.FACEBOOK_APP_ID;
   next();
 });
 
