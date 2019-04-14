220fc66b675ecf0334d32519f6724a3a69af984f
Miguel <miguel@sopinet.com>
2019-04-14 17:52:28 +0200

    Fix article initial state

--- admin/src/components/article/list/Article.js
+++ admin/src/components/article/list/Article.js
@@ -7,7 +7,7 @@ const Article = (props, context) => {
   const {title, summary, _id, file_name, state} = props.article;
   return (
       <Row as={Col} xs={12}>
-        <Col xs={10} md={4} lg={3}>
+        <Col xs={6} md={6} lg={3}>
           <p>{title}</p>
         </Col>
         <Col className='d-none d-lg-block' lg={4}>
@@ -16,8 +16,12 @@ const Article = (props, context) => {
         <Col className='d-none d-md-block' md={2}>
           <img src={file_name} alt={title}/>
         </Col>
-        <Col className='d-none d-md-block' md={1}>
+        <Col xs={3} md={2} lg={1}>
           <p>{state ? context.t('Published') : context.t('Draft')}</p>
+        </Col>
+        <Col xs={3} md={2} lg={1}>
+          <button>{context.t('Edit')}</button>
+          <button>{context.t('Remove')}</button>
         </Col>
       </Row>
   );
--- admin/src/components/article/list/ArticleList.js
+++ admin/src/components/article/list/ArticleList.js
@@ -24,7 +24,7 @@ const ArticleList = (props) => {
 
   useEffect(() => {
     fetchArticles();
-  }, []);
+  }, articles);
 
   return(
       <Row>
