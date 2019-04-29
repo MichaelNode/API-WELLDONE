f80ccc15189093b26f2af2f2ccee56c9d9039906
miguel <miguel@sopinet.com>
2019-04-29 20:16:37 +0200

    Added mini css extract plugin

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,14 @@ require("@babel/polyfill");
 module.exports = {
     entry: ["@babel/polyfill", "./public/javascripts/app.js"],
     mode: 'development',
+    plugins: [
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // both options are optional
+            filename: "[name].css",
+            chunkFilename: "[id].css"
+        })
+    ],
     module: {
         rules: [
             {
@@ -16,18 +24,26 @@ module.exports = {
             },
             {
                 test: /\.s(a|c)ss$/,
-                use: [{
-                    loader: "style-loader"
-                }, {
-                    loader: "css-loader",
-                }, {
-                    loader: "postcss-loader",
-                    options: {
-                        plugins: () => [require('precss'), require('autoprefixer')]
-                    }
-                }, {
-                    loader: "sass-loader"
-                }]
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            // you can specify a publicPath here
+                            // by default it uses publicPath in webpackOptions.output
+                            publicPath: '../',
+                            hmr: process.env.NODE_ENV === 'development',
+                        },
+                    },
+                    {
+                        loader: "css-loader",
+                    }, {
+                        loader: "postcss-loader",
+                        options: {
+                            plugins: () => [require('precss'), require('autoprefixer')]
+                        }
+                    }, {
+                        loader: "sass-loader"
+                    }]
             },
             {
                 test: /\.(png|woff|woff2|eot|ttf|svg)$/,
@@ -35,12 +51,4 @@ module.exports = {
             }
         ]
     },
-    plugins: [
-        new MiniCssExtractPlugin({
-            // Options similar to the same options in webpackOptions.output
-            // both options are optional
-            filename: "[name].css",
-            chunkFilename: "[id].css"
-        })
-    ],
 }
